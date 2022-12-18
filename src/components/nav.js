85ce9904d769a801ec370d851e4ede4c2384910c
Keshav Jha <keshavjha0610@gmail.com>
2022-12-18 18:47:00 +0530

    modified nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -294,7 +294,7 @@ class Nav extends Component {
                 <CSSTransition classNames={fadeDownClass} timeout={timeout}>
                   <div style={{ transitionDelay: `${isHome ? navLinks.length * 100 : 0}ms` }}>
                     <StyledResumeButton
-                      href="/Keshav_SDET_3.3 YOE.pdf"
+                      href="/Keshav_SDET.pdf"
                       target="_blank"
                       rel="nofollow noopener noreferrer">
                       Resume
